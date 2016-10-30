8fff33fa76f238b15387ca7bc6357ce98bea3a5d
Michal Kononenko <michalkononenko@gmail.com>
2016-10-30 15:42:59 -0400

    Added source for a simple HelloWorld app

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * The main runner for the application
+ */
+import HelloWorld.HelloWorld;
+
+public class Main extends HelloWorld {
+    public static void main(String[] args){
+        launch(args);
+    }
+}
