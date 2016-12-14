29e9735f59570c0062a505192a0358f38b1df578
MichalKononenko <travis@travis-ci.org>
2016-12-14 14:40:06 -0500

    Refactored array test to yield some actual values.

--- src/test/java/unit/kernel/Kernel.java
+++ src/test/java/unit/kernel/Kernel.java
@@ -7,6 +7,11 @@ import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.Enumeration;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Tests that the kernel is successfully able to get names of ports
  */
@@ -20,13 +25,43 @@ public class Kernel {
         ICommPortManager manager = context.mock(ICommPortManager.class);
 
         kernel.Kernel kernel = new kernel.Kernel(manager);
+        MockComPortList portNames = new MockComPortList();
 
         context.checking(new Expectations() {{
             oneOf(manager).getCommPortNames();
+            will(returnValue(portNames));
         }});
 
-        kernel.getCommPortNames();
-
+        assertEquals(portNames.getElements(), kernel.getCommPortNames());
         context.assertIsSatisfied();
+    }
+}
+
+class MockComPortList implements Enumeration {
+    private int index;
+    private ArrayList<String> data;
+    private int size;
+
+    MockComPortList(){
+        index = 0;
+        data = new ArrayList<>();
+        data.add("COM1");
+        data.add("COM2");
+
+        size = data.size();
+    }
+
+    @Override public String nextElement() {
+        String value_to_return = data.get(index);
+        this.index += 1;
+        return value_to_return;
+    }
+
+    @Override public boolean hasMoreElements(){
+        return index < size;
+    }
+
+    public ArrayList<String> getElements(){
+        return data;
     }
 }
