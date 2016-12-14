9f46191b6f351fdf6848143162bde78d5212184c
MichalKononenko <travis@travis-ci.org>
2016-12-14 17:55:30 -0500

    Refactored Comm Port Managers to include a stub class.

--- src/test/java/unit/kernel/kernel_factory/SetCommPortManager.java
+++ src/test/java/unit/kernel/kernel_factory/SetCommPortManager.java
@@ -5,15 +5,41 @@ import kernel.ApplicationKernelFactory;
 import kernel.comm_port_manager.CommPortManager;
 import org.junit.Test;
 
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Contains methods for
  * {@link ApplicationKernelFactory#setCommPortManager(CommPortManager)}
  */
 public class SetCommPortManager extends TestKernelFactory {
-    @Test public void testSetter(){
-        applicationKernelFactory.setCommPortManager(commPortManager);
+    private MockCommPortManager newManager;
+
+    public SetCommPortManager(){
+        ArrayList<String> portNames = new ArrayList<>();
+        portNames.add("COM1");
+        portNames.add("COM2");
+
+        this.newManager = new MockCommPortManager(portNames);
     }
 
+    /**
+     * Tests that the port manager can be switched out for any manager that
+     * implements {@link CommPortManager}
+     */
+    @Test public void testSetter(){
+        applicationKernelFactory.setCommPortManager(newManager);
+
+        assertEquals(
+            newManager, applicationKernelFactory.getCommPortManager()
+        );
+    }
+
+    /**
+     * Tests that {@link UnableToSetParameterException} is thrown if the
+     * manager tries to be set while running the kernel
+     */
     @Test(expected= UnableToSetParameterException.class)
     public void testSetterWithError(){
         this.startKernel();
