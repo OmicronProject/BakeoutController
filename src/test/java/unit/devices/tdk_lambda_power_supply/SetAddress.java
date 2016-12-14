bfffb54e38e6c2596539ddf35d4eecf2e4779def
MichalKononenko <travis@travis-ci.org>
2016-12-14 18:03:20 -0500

    Added a unit test to test address too low conditional

--- src/test/java/unit/devices/tdk_lambda_power_supply/SetAddress.java
+++ src/test/java/unit/devices/tdk_lambda_power_supply/SetAddress.java
@@ -10,16 +10,18 @@ import static org.junit.Assert.assertEquals;
  * Contains unit tests for {@link TDKLambdaPowerSupply#setAddress(int)}
  */
 public class SetAddress extends TestTDKLambdaPowerSupplyWithFixture {
-    private int goodAddress = 10;
-    private int badAddress = 300;
+    private static final int goodAddress = 10;
+    private static final int badAddressTooHigh = 300;
+    private static final int badAddressTooLow = -10;
 
     @Test public void testSetGoodAddress(){
         supply.setAddress(goodAddress);
         assertEquals(goodAddress, supply.getAddress());
     }
 
-    @Test(expected=NotAllowedAddressException.class)
-    public void testSetBadAddress(){
-        supply.setAddress(badAddress);
-    }
+    @Test(expected = NotAllowedAddressException.class)
+    public void testSetHighAddress(){ supply.setAddress(badAddressTooHigh); }
+
+    @Test(expected = NotAllowedAddressException.class)
+    public void testSetLowAddress() { supply.setAddress(badAddressTooLow); }
 }
